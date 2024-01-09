5bdadf43a1f27be5f9a644a0c06d61754d1b4c26
Blushister <59022588+Blushister@users.noreply.github.com>
2024-01-09 08:17:56 +0100

    Update import.py

--- import.py
+++ import.py
@@ -5,17 +5,19 @@ import pandas as pd
 
 # Informations de connexion sans spécifier la base de données
 db_config = {
-    'user': 'curveo',
-    'password': 'melodie2012',
-    'host': 'localhost',
+    'user': 'moveia',
+    'password': 'rqUuHDulPaQGePS',
+    'host': 'los-santos.fr',
     'raise_on_warnings': True
 }
 
 # Connexion initiale à MySQL
+print("Connexion initiale à la base de données.")
 cnx = mysql.connector.connect(**db_config)
 cursor = cnx.cursor()
 
 # Création de la base de données
+print("Création de la base de données 'moveia' si elle n'existe pas.")
 cursor.execute("CREATE DATABASE IF NOT EXISTS moveia;")
 cursor.close()
 cnx.close()
@@ -24,10 +26,12 @@ cnx.close()
 db_config['database'] = 'moveia'
 
 # Reconnexion avec la base de données spécifiée
+print("Reconnexion avec la base de données 'moveia'.")
 cnx = mysql.connector.connect(**db_config)
 cursor = cnx.cursor()
 
 # Script pour créer les tables
+print("Création des tables dans la base de données.")
 create_tables_script = """
 CREATE TABLE IF NOT EXISTS Movies (
     movie_id INT PRIMARY KEY,
@@ -64,7 +68,6 @@ CREATE TABLE IF NOT EXISTS cast (
     movie_id INT,
     actor_name VARCHAR(255),
     character_name VARCHAR(800)
-    -- Remplacer par d'autres colonnes si nécessaire --
 );
 
 CREATE TABLE IF NOT EXISTS crew (
@@ -72,18 +75,20 @@ CREATE TABLE IF NOT EXISTS crew (
     movie_id INT,
     person_name VARCHAR(255),
     role VARCHAR(255)
-    -- Remplacer par d'autres colonnes si nécessaire --
 );
 """
-
-# Exécuter le script de création des tables
 for result in cursor.execute(create_tables_script, multi=True):
-  pass  # Nécessaire pour exécuter multi-statements
+    pass
+
+print("Tables créées avec succès.")
 
 combined_df = pd.read_csv('csv/combined_movies_data.csv')
 
-# Insérer des données dans la table Movies
-for _, row in combined_df.iterrows():
+print("Début de l'insertion des données dans la base.")
+cnx.start_transaction()
+
+for index, row in combined_df.iterrows():
+    print(f"Insertion du film ID {row['id']}: {row['original_title']}")
     cursor.execute("""
     INSERT INTO Movies (movie_id, title, overview, release_date, budget, revenue, runtime, vote_average, vote_count)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
@@ -101,35 +106,40 @@ for _, row in combined_df.iterrows():
 
     genres = json.loads(row['genres'].replace("'", "\""))
     for genre in genres:
+        print(f"  Traitement du genre {genre['name']} pour le film {row['original_title']}")
         cursor.execute("""
             INSERT INTO Genres (genre_id, name) 
-            VALUES (%s, %s) AS new_genre
-            ON DUPLICATE KEY UPDATE name = new_genre.name
+            VALUES (%s, %s)
+            ON DUPLICATE KEY UPDATE name = VALUES(name)
         """, (genre['id'], genre['name']))
         cursor.execute("""
             INSERT INTO MovieGenres (movie_id, genre_id) 
-            VALUES (%s, %s) AS new_movie_genre
-            ON DUPLICATE KEY UPDATE movie_id = new_movie_genre.movie_id, genre_id = new_movie_genre.genre_id
+            VALUES (%s, %s)
+            ON DUPLICATE KEY UPDATE movie_id = VALUES(movie_id), genre_id = VALUES(genre_id)
         """, (row['id'], genre['id']))
 
-for index, row in combined_df.iterrows():
-    # Extraction et transformation de la colonne 'cast'
+    # Traitement de la colonne 'cast'
     if not pd.isna(row['cast']):
         cast_list = json.loads(row['cast'])
         for cast_member in cast_list:
-            insert_cast_query = "INSERT INTO cast (movie_id, actor_name, character_name) VALUES (%s, %s, %s)"
-            cursor.execute(insert_cast_query, (row['id'], cast_member['name'], cast_member['character']))
+            print(f"  Ajout de l'acteur {cast_member['name']} dans le film {row['original_title']}")
+            cursor.execute("""
+                INSERT INTO cast (movie_id, actor_name, character_name) 
+                VALUES (%s, %s, %s)
+            """, (row['id'], cast_member['name'], cast_member['character']))
 
-    # Extraction et transformation de la colonne 'crew'
+    # Traitement de la colonne 'crew'
     if not pd.isna(row['crew']):
         crew_list = json.loads(row['crew'])
         for crew_member in crew_list:
-            insert_crew_query = "INSERT INTO crew (movie_id, person_name, role) VALUES (%s, %s, %s)"
-            cursor.execute(insert_crew_query, (row['id'], crew_member['name'], crew_member['job']))
+            print(f"  Ajout du membre de l'équipe {crew_member['name']} dans le film {row['original_title']}")
+            cursor.execute("""
+                INSERT INTO crew (movie_id, person_name, role) 
+                VALUES (%s, %s, %s)
+            """, (row['id'], crew_member['name'], crew_member['job']))
 
-# Commit les opérations
 cnx.commit()
+print("Insertion des données terminée.")
 
-# Fermer la connexion
 cursor.close()
 cnx.close()

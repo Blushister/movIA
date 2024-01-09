35aa1723702b4192e409908f392dc44212675a22
Arnaud <arnaud@neuralvision.fr>
2024-01-09 14:23:14 +0100

    commit random

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,37 +1,38 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
-import mysql from 'mysql2';
+import mysql from "mysql2";
 
 dotenv.config();
-const port = 3000;
-const name = 'movIA';
-const version = '1.0.0';
+const port = 3001;
+const name = "movIA";
+const version = "1.0.0";
 
 const dbConfig = {
-    host: 'los-santos.fr',
-    user: 'moveia',
-    password: 'rqUuHDulPaQGePS',
-    database: 'moveia',
+  host: "los-santos.fr",
+  user: "moveia",
+  password: "rqUuHDulPaQGePS",
+  database: "moveia",
 };
 
 const connection = mysql.createConnection(dbConfig);
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MariaDB:', err);
-        return;
-    }
-    console.log('Connected to MariaDB');
+  if (err) {
+    console.error("Error connecting to MariaDB:", err);
+    return;
+  }
+  console.log("Connected to MariaDB");
 });
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.get('/', (_req: Request, res: Response) => { res.send(`Api: ${name} \n version: ${version}`)
+server.get("/", (_req: Request, res: Response) => {
+  res.send(`Api: ${name} \n version: ${version}`);
 
-    /*const query = 'SELECT * FROM Movies';
+  /*const query = 'SELECT * FROM Movies';
 
     connection.query(query, (err, results) => {
         if (err) {
@@ -43,11 +44,11 @@ server.get('/', (_req: Request, res: Response) => { res.send(`Api: ${name} \n ve
     });*/
 });
 
-process.on('SIGINT', () => {
-    connection.end();
-    process.exit();
+process.on("SIGINT", () => {
+  connection.end();
+  process.exit();
 });
 
-server.listen(port, () =>{
-    console.log(`${name} on http://localhost:${port}/ `);
+server.listen(port, () => {
+  console.log(`${name} on http://localhost:${port}/ `);
 });
